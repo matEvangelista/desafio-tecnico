acb476149420d3f965c0f8e5544a5e20b6b8c041
Mateus Alcantara <mateus.e.alcantara@outlook.com>
2023-04-29 07:49:14 -0300

    indicações readme

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+    // mais detalhes no README.md
     return (
         <>
             <Navbar />
